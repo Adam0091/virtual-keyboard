1ab7b097226caf2a072441a0c4804d106bb6bf7c
Adam <adam.holo@mail.ru>
2022-05-04 02:12:03 +0400

    feat: add main.js

---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+import KeyboardKeys from "./components/KeyboardKeys";
+import KeyboardEvents from "./events/KeyboardEvents";
+
+const root = document.createElement("div");
+root.classList.add("root");
+
+root.innerHTML = `
+<div class="page">
+    <div class="keyboard">
+        <div class="container keyboard__container">
+            <h1 class="keyboard__title">Virtual keyboard</h1>
+            <textarea class="keyboard__textarea" name="" id="keyboard_textarea"></textarea>
+            <div class="keyboard__keys">${KeyboardKeys().innerHTML}</div>
+        </div>
+    </div>
+</div>
+`;
+
+document.body.append(root);
+
+const keyboardEvents = new KeyboardEvents();
+document.addEventListener("click", keyboardEvents);
+document.addEventListener("keydown", keyboardEvents);
+document.addEventListener("keyup", keyboardEvents);
