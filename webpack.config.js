2b6475d1df79b266b76be69bbb0984a45e81c65d
Adam <adam.holo@mail.ru>
2022-05-03 12:02:40 +0400

    feat: add initial build

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "./assets/js/[name].js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.pug$/i,
+        loader: "pug-loader",
+        options: { pretty: true },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          { loader: "resolve-url-loader" },
+          { loader: "sass-loader", options: { sourceMap: true } },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|otf|woff2?)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/fonts/[name][ext]" },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|webp|ico)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/images/[name][ext]" },
+      },
+      {
+        test: /\.(mp4)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/videos/[name][ext]" },
+      },
+      {
+        test: /\.(mp3)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/media/[name][ext]" },
+      },
+      {
+        test: /\.(docx?|xlsx?|pptx?|pdf)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/docs/[name][ext]" },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/pages/index.html",
+      inject: "body",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./assets/css/main.css",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./src/.htaccess", to: "./" }],
+    }),
+  ],
+};
