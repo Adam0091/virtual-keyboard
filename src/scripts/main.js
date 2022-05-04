12aa3b68b1030425c54f209326980ba050b7cb50
Adam <adam.holo@mail.ru>
2022-05-04 18:51:56 +0400

    feat: add subtitles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,8 @@ root.innerHTML = `
     <div class="keyboard">
         <div class="container keyboard__container">
             <h1 class="keyboard__title">Virtual keyboard</h1>
+            <h2 class="keyboard__subtitle">Made on Windows</h2>
+            <h2 class="keyboard__subtitle">Combination change keyboard layout: CTRL + ALT</h2>
             <textarea class="keyboard__textarea" name="" id="keyboard_textarea"></textarea>
             <div class="keyboard__keys">${KeyboardKeys().innerHTML}</div>
         </div>
@@ -17,6 +19,8 @@ root.innerHTML = `
 `;
 
 document.body.append(root);
+
+document.querySelector(".keyboard__textarea").onkeypress = () => false;
 
 const keyboardEvents = new KeyboardEvents();
 document.addEventListener("click", keyboardEvents);
